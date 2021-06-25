8c035ac6d62b2858243791d2fb87e36f27509712
User <User@DESKTOP-OC97NIL>
2021-06-25 07:17:29 -0700

    commit

---
+++ mynewjavaproject/src/edu/vcentry/basics/session05/forloop/For.java
@@ -0,0 +1,22 @@
+package edu.vcentry.basics.session05.forloop;
+
+public class For {
+
+	public static void main(String[] args) {
+
+
+	
+
+	
+	for(int num = 20;num<=30;num=num+1) {
+	     if(num%2==0) {
+	    	 
+	    	 System.out.println(num);
+	     }
+		
+	}		
+	}
+	
+}
+
+
